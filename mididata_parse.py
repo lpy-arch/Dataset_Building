960a1a77d2f752681452e1014927da763708eaca
lpy-arch <2745343915@qq.com>
2024-04-21 17:19:10 +0800

    buited dataset_small, ready to merge duplicated tracks

--- mididata_parse.py
+++ mididata_parse.py
@@ -2,6 +2,7 @@ from music21 import *
 import pretty_midi
 from collections import Counter
 import os
+import shutil
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -132,7 +133,12 @@ def count_instument_under_condition(all_file_path):
                 instrument_in_song.sort()
 
                 ###############################条件在这里###############################
-                if all(item in instrument_in_song for item in ['Drum', 'bass']) and (any(item in instrument_in_song for item in ['clean_guitar', 'electric_guitar'])):
+                # drum、bass、两种guitar有一个就好的条件筛选
+                # if all(item in instrument_in_song for item in ['Drum', 'bass']) and (any(item in instrument_in_song for item in ['clean_guitar', 'electric_guitar'])):
+
+                # drum、bass、clean_guitar、electric_guitar、vocal均有的条件筛选
+                if all(item in instrument_in_song for item in ['Drum', 'bass', 'clean_guitar', 'electric_guitar', 'vocal']):
+
                     effecitve += 1
                     # print(f"{instrument_in_song}")
     
@@ -142,10 +148,69 @@ def count_instument_under_condition(all_file_path):
 
     return effecitve
 
+def select_small_dataset(all_file_path, target_dir):
+    selected_amount = 0
+    for file in tqdm(all_file_path, desc='Processing', unit='file'):
+        try:
+                # 用pretty_midi解析mid文件时是要"try"的，因为可能会有不符合规范的mid文件
+                midi_file = pretty_midi.PrettyMIDI(file)
+                # 获取mid文件中所有的乐器，是所有乐器组成的列表
+                instruments = midi_file.instruments
+
+                # 创建一个列表储存这首歌里的音轨
+                instrument_in_song = []
+
+                # 遍历每一个乐器
+                for item in instruments:
+                    # 先把lable设定成others，后面如果发现是想要的乐器类型的话，就改过来了
+                    instrument_lable = "others"
+                    # 判断是否是鼓，因为鼓的编号不在标准音色表里
+                    if item.is_drum:
+                        instrument_lable = "Drum"
+                        instrument_in_song.append(instrument_lable)
+                    else:
+                        # 标准音色编号到乐器类别的映射
+                        instrument_map = {
+                            "vocal": [56, 57, 58, 59, 60, 61, 62, 63, 80, 81, 82, 83, 84, 85, 86, 87, 73, 71, 66, 65, 40],
+                            "piano": [0, 1, 2, 3, 4, 5, 6, 7],
+                            "clean_guitar": [26, 27, 24, 25, 31],
+                            "electric_guitar": [28, 29, 30],
+                            "bass": [32, 33, 34, 35, 36, 37, 38, 39]
+                            }
+                        # 如果编号在映射里，就更新lable
+                        for lable, numbers in instrument_map.items():
+                            if item.program in numbers:
+                                instrument_lable = lable
+                                instrument_in_song.append(instrument_lable)
+                # 输出一下音乐中的乐器组成看看
+                instrument_in_song = list(set(instrument_in_song))  # 无重复元素
+                instrument_in_song.sort()
+
+                ###############################条件在这里###############################
+                # drum、bass、clean_guitar、electric_guitar、vocal均有的条件筛选
+                if all(item in instrument_in_song for item in ['Drum', 'bass', 'clean_guitar', 'electric_guitar', 'vocal']):
+                    # 获取源文件和新的文件路径
+                    old_path = file
+                    parts = old_path.split(os.sep)
+                    new_path = os.path.join(target_dir, parts[2], parts[-1])
+                    # 创建指定的文件夹
+                    new_dir = os.path.dirname(new_path)
+                    if not os.path.exists(new_dir):
+                        os.makedirs(new_dir)
+                    # 将文件转存入新的路径下
+                    midi_file = pretty_midi.PrettyMIDI(old_path)
+                    midi_file.write(new_path)
+                    selected_amount += 1
+    
+        except Exception as e:
+            print(f"Error processing item {item}: {e}")
+            continue
+    return selected_amount
+
 
 if __name__ == "__main__":
-    # 处理的文件夹
-    directory = "dataset_mid/Mysongbook/Pop_Rock"
+    # 指定处理的文件夹
+    directory = "dataset_small"
 
     # 获取所有的mid文件列表
     all_file_path = []
@@ -156,12 +221,15 @@ if __name__ == "__main__":
                 all_file_path.append(file_path)
 
     # 数据集再小一点，只截取[0:500]
-    all_file_path = all_file_path[0:-1]
+    all_file_path = all_file_path[0:20]
 
     # 统计乐器分布图
     # plot_instrument_distrubution(all_file_path, "未命名.jpg")
 
     # 统计符合条件的文件数量
-    amount = count_instument_under_condition(all_file_path)
-    print(f"File amount under the condition:{amount}")
+    # amount = count_instument_under_condition(all_file_path)
+    # print(f"File amount under the condition:{amount}")
 
+    # 将符合乐轨条件的文件转存到新的文件夹中
+    # selected_amount = select_small_dataset(all_file_path, target_dir="dataset_small")
+    # print(f"File amount under the condition:{selected_amount}")
